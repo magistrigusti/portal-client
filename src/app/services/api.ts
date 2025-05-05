263dcda9990fea2c53f750ba35c482d6c8cb274b
magistrigusti <47912728+magistrigusti@users.noreply.github.com>
2025-05-05 21:07:58 +0300

    0.1.04

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -1,18 +1,18 @@
-import { fetchBaseQuery, createApi, retry } from "@reduxjs/toolkit/query";
+import { fetchBaseQuery, createApi, retry } from "@reduxjs/toolkit/query/react";
 import { BASE_URL } from "../../constants";
-import { RootState } from "../store";
+// import { RootState } from "../store";
 
 const baseQuery = fetchBaseQuery({
   baseUrl: `${BASE_URL}/api`,
-  prepareHeaders: (headers, { getState }) => {
-    const token= (getState() as RootState).auth.token || localStorage.getItem("token");
+  // prepareHeaders: (headers, { getState }) => {
+  //   const token= (getState() as RootState).auth.token || localStorage.getItem("token");
 
-    if (token) {
-      headers.set('authorization', `Bearer ${token}`)
-    }
+  //   if (token) {
+  //     headers.set('authorization', `Bearer ${token}`)
+  //   }
 
-    return headers;
-  }
+  //   return headers;
+  // }
 });
 
 const baseQueryWithRetry = retry(baseQuery, { maxRetries: 1 })
--- src/app/services/userApi.ts
+++ src/app/services/userApi.ts
@@ -1,4 +1,5 @@
 import { api } from './api';
+import { User } from '../types';
 
 export const userApi = api.injectEndpoints({
   endpoints: (builder) => ({
@@ -21,6 +22,39 @@ export const userApi = api.injectEndpoints({
         method: 'POST',
         body: userData
       })
-    })
+    }),
+    current: builder.query<User, void>({
+      query: () => ({
+        url: '/current',
+        method: 'GET'
+      })
+    }),
+    getUserById: builder.query<User, string>({
+      query: (id) => ({
+        url: `/users/${id}`,
+        method: 'GET'
+      })
+    }),
+    updateUser: builder.mutation<User, { userData: FormData, id: string}>({
+      query: ({userData, id}) => ({
+        url: `/users/${id}`,
+        method: 'PUT',
+        body: userData
+      })
+    }),
   })
 });
+
+export const {
+  useRegisterMutation,
+  useLoginMutation,
+  useCurrentQuery,
+  useLazyCurrentQuery,
+  useGetUserByIdQuery,
+  useLazyGetUserByIdQuery,
+  useUpdateUserMutation
+} = userApi
+
+export const {
+  endpoints: { login, register, current, getUserById, updateUser }
+} = userApi;  
