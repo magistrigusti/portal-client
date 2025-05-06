896b15e3eb1d6ad0129286d629175e266cb38b94
magistrigusti <47912728+magistrigusti@users.noreply.github.com>
2025-05-06 17:23:29 +0300

    0.2.03

---
+++ src/components/header/index.tsx
@@ -0,0 +1,29 @@
+import React, { useContext } from 'react';
+import { ThemeContext } from '../theme-provider';
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem } from '@nextui-org/react';
+import { FaRegMoon } from 'react-icons/fa';
+import { LuSunMedium } from 'react-icons/lu';
+
+export const Header = () => {
+  const { theme, toggleTheme } = useContext(ThemeContext);
+
+  return (
+    <Navbar>
+      <NavbarBrand>
+        <p className="font-bold text-inherit">
+          Portal Social Network
+        </p>
+      </NavbarBrand>
+
+      <NavbarContent justify="end">
+        <NavbarItem className="lg:flex text-3xl cursor-pointer">
+          {theme === 'light' ? <FaRegMoon /> : <LuSunMedium />}
+        </NavbarItem>
+
+        <NavbarItem>
+          
+        </NavbarItem>
+      </NavbarContent>
+    </Navbar>
+  )
+}
