a02c33147a59288e5f5d6ddb4a89840e7468c315
magistrigusti <47912728+magistrigusti@users.noreply.github.com>
2025-05-13 16:11:20 +0300

    0.2.04

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,9 +1,16 @@
-
+import { Header } from '../header';
+import { Container } from '../container';
 
 export const Layout = () => {
   return (
     <>
-    
+      <Header />
+
+      <Container>
+        <div className="flex-2-p4">
+          ...
+        </div>
+      </Container>
     </>
   )
 }
