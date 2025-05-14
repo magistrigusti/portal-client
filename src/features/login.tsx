89f2cf0e08042925b389c614ca0bc2632dfc4e4f
magistrigusti <47912728+magistrigusti@users.noreply.github.com>
2025-05-14 11:47:30 +0300

    0.3.09

---
+++ src/features/login.tsx
@@ -0,0 +1,12 @@
+
+type Props = {
+  setSelected: (value: string) => void;
+}
+
+export const Login = () => {
+  return (
+    <div>
+      Login
+    </div>
+  )
+}
--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,9 +1,30 @@
-
+import { useState } from 'react';
+import { Card, CardBody, Tabs, Tab } from '@nextui-org/react';
 
 export const Auth = () => {
+  const [selected, setSelected] = useState('login');
+
   return (
-    <div>
-      Auth
+    <div className="flex items-center justify-center h-screen">
+      <div className="flex flex-col">
+        <Card className="max-w-full w-[340px] h-[450px]">
+          <CardBody className="overwlow-hidden">
+            <Tabs fullWidth
+              size="md"
+              selectedKey={ selected }
+              onSelectionChange={(key) => setSelected(key as string)}
+            >
+              <Tab key="login" title="Enter">
+                Enter
+              </Tab>
+
+              <Tab key="sign-up" title="sigh-up">
+                Sigh Up
+              </Tab>
+            </Tabs>
+          </CardBody>
+        </Card>
+      </div>
     </div>
   )
 }
